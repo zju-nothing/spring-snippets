99c755609287b4b7c5e02332444ae47bc7fb7e34
纳信 <chenliu.cl@alibaba-inc.com>
2014-11-10 22:41:33 +0800

    init the snippet-01: the very first servlet demo

---
+++ spring-snippet-01/README.md
@@ -0,0 +1,3 @@
+This is the very first of the demo
+* 提供部署脚本deploy.sh,自动部署到tomcat中，war名字是spring-snippet-01.war
+* web.xml中有一个servlet，部署后访问 localhost:8080/spring-snippet-01/hello
---
+++ spring-snippet-01/src/main/java/com/nothing/servlet/HelloServlet.java
@@ -0,0 +1,22 @@
+package com.nothing.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author chenliu, create at 11/10/14 22:11
+ */
+public class HelloServlet extends HttpServlet{
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<h1>Hello Servlet Get</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
